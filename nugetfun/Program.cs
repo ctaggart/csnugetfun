3e84ad69b8cf12b10a211d1c679547c7b7bdc00e
Cameron Taggart <cameron.taggart@virtustream.com>
2019-04-19 16:07:18 -0500

    console app from template with locking

---
+++ nugetfun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nugetfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
