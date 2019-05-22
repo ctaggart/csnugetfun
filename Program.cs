7dae8dc7f6158932aab52e789c9622786a29eb2c
Cameron Taggart <cameron.taggart@virtustream.com>
2019-05-22 23:02:35 +0700

    mkbundlefun

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace nugetfun
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var rsp = await client.GetAsync("https://reqres.in/api/users/2");
+            Console.WriteLine("http response status code: " + rsp.StatusCode);
+        }
+    }
+}
